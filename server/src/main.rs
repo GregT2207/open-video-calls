146663f14ccfe210f2adbf05f137b3ca6b9bf9f1
greg <gregory.tuck@hotmail.com>
2025-06-10 17:31:47 +0100

    Avoid time comparison error case

--- server/src/main.rs
+++ server/src/main.rs
@@ -40,8 +40,9 @@ fn remove_old_connections(connections: &mut HashMap<SocketAddr, SystemTime>) {
     let expired: Vec<SocketAddr> = connections
         .iter()
         .filter_map(|(address, last_received)| {
-            if now.duration_since(*last_received).unwrap()
-                > Duration::from_secs(CONNECTION_EXPIRY_TIME)
+            if *last_received > now
+                && now.duration_since(*last_received).unwrap()
+                    > Duration::from_secs(CONNECTION_EXPIRY_TIME)
             {
                 Some(*address)
             } else {
