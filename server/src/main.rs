9d27281cb01e1ea56860d37a1eeac47f42369652
greg <gregory.tuck@hotmail.com>
2025-06-10 14:19:23 +0100

    Manage connections and forward incoming packets to all other connections

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use core::net::SocketAddr;
+use std::{
+    collections::HashMap,
+    net::UdpSocket,
+    time::{Duration, SystemTime},
+};
+
+const SERVER_ADDRESS: &str = "127.0.0.1";
+const SERVER_PORT: &str = "5000";
+const CONNECTION_EXPIRY_TIME: u64 = 10;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind(format!("{}:{}", SERVER_ADDRESS, SERVER_PORT))
+        .expect("Failed to bind to UDP socket");
+    let mut buffer = [0u8; 1500];
+    let mut connections: HashMap<SocketAddr, SystemTime> = HashMap::new(); // Address and time last packet was received
+
+    loop {
+        let (size, src) = socket.recv_from(&mut buffer)?;
+        let data = &mut buffer[..size];
+
+        connections
+            .entry(src)
+            .and_modify(|t| *t = SystemTime::now())
+            .or_insert(SystemTime::now());
+
+        for (address, _) in &connections {
+            if address == &src {
+                continue;
+            }
+            socket.send_to(data, address)?;
+        }
+
+        remove_old_connections(&mut connections);
+    }
+}
+
+fn remove_old_connections(connections: &mut HashMap<SocketAddr, SystemTime>) {
+    let now = SystemTime::now();
+    let expired: Vec<SocketAddr> = connections
+        .iter()
+        .filter_map(|(address, last_received)| {
+            if now.duration_since(*last_received).unwrap()
+                > Duration::from_secs(CONNECTION_EXPIRY_TIME)
+            {
+                Some(*address)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    for address in expired {
+        connections.remove(&address);
+    }
 }
